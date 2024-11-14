246783cda1728ced992efc272ff0a8e5cdc909fb
CarlosOboria <carloslucientes@oboria.com>
2024-11-14 14:07:13 +0100

    working on sunshine

--- src/searchApp.js
+++ src/searchApp.js
@@ -10,49 +10,48 @@ class SearchApp extends TeamsActivityHandler {
   }
 
   // Message extension Code
-  // Search.
   async handleTeamsMessagingExtensionQuery(context, query) {
-    // get the search query
-    //there is a single search parameter (query.parameters[0]) being accessed and used as the search query
+    // Get the search query
     const searchQuery = query.parameters[0].value.toLowerCase();
-    // execute search logic
+
+    // Execute search logic
+    const response = await axios.get(`https://rickandmortyapi.com/api/character/?${querystring.stringify({
+      name: searchQuery
+    })}`);
     
-      // Ejecutar la consulta en PokeAPI
-      const response = await axios.get(`https://rickandmortyapi.com/api/character/?name=${searchQuery}`);
+    console.log("response es:", response);
 
-      
+    // Filter the results
+    const characterDetails = response.data.results[0];
+    console.log("characterDetails es:", characterDetails);
 
-      // Filtrar los resultados según el término de búsqueda
-      const characterDetails = response.data.results[0];
-      console.log("characterDetails es : ",characterDetails)
+    if (!characterDetails) {
+      throw new Error("No se encontró ningún personaje con ese nombre.");
+    }
 
-      if (!characterDetails) {
-        throw new Error("No se encontró ningún personaje con ese nombre.");
-      }
+    // Create a Hero Card for the preview
+    const preview = CardFactory.heroCard(
+      characterDetails.name.toUpperCase(),
+      `status: ${characterDetails.status} | Gender: ${characterDetails.gender}`,
+      [`https://rickandmortyapi.com/api/character/avatar/${characterDetails.id}.jpeg`]
+    );
 
-      // Crear una tarjeta de héroe con la información básica del Pokémon
-  const preview = CardFactory.heroCard(
-    characterDetails.name.toUpperCase(),
-    `status: ${characterDetails.status} | Gender: ${characterDetails.gender}`,
-    [`https://rickandmortyapi.com/api/character/avatar/${characterDetails.id}.jpeg`]
-  );
-  
-    // Configurar el tap para abrir los detalles del personaje
-    preview.content.tap = {
-      type: "invoke",
-      value: {
+    // Create an Adaptive Card based on the template
+    const template = new ACData.Template(rickVerso);
+    const card = template.expand({
+      $root: {
         id: characterDetails.id,
         name: characterDetails.name,
         status: characterDetails.status,
         gender: characterDetails.gender,
         species: characterDetails.species
-      }
-    };
+      },
+    });
 
-    // Crear el attachment con la heroCard
-    const attachment = { ...preview };
+    // Combine Hero Card and Adaptive Card in the attachment
+    const attachment = { ...CardFactory.adaptiveCard(card), preview };
 
-    // Devolver los resultados usando heroCard
+    // Return the results using the attachment
     return {
       composeExtension: {
         type: "result",
